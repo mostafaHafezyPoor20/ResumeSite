2063155f7c1507cdbe6d69aa339e7f0794b47a01
mostafa hafezypoor <m.folad1383@gmail.com>
2026-01-04 20:39:43 +0330

    write api section about me !

---
+++ app/Controllers/Api/Admin/getAboutMe.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_POST["key"])){
+    require_once ("../../../Config/connDB.php");
+    if ($_POST["key"]==KEY){
+      $titleAboutMe=file_get_contents("../../../Storage/titleAboutMe.txt");
+      $descriptionAboutMe=file_get_contents("../../../Storage/descriptionAboutMe.txt");
+      $address=file_get_contents("../../../Storage/address.txt");
+      $email=file_get_contents("../../../Storage/email.txt");
+      $phoneNumber=file_get_contents("../../../Storage/phoneNumber.txt");
+      echo json_encode([
+          "titleAboutMe"=>$titleAboutMe,
+          "descriptionAboutMe"=>$descriptionAboutMe,
+          "address"=>$address,
+          "email"=>$email,
+          "phoneNumber"=>$phoneNumber
+      ]);
+    }
+}
+
+?>
