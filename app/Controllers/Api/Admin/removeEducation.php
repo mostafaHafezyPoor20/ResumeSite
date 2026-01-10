29f154fb968019c0ee0f3127c34c29948c2d6f12
mostafa hafezypoor <m.folad1383@gmail.com>
2026-01-10 10:13:23 +0330

    write api section education

---
+++ app/Controllers/Api/Admin/removeEducation.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_POST["key"])&&isset($_POST["id"])){
+    require_once ("../../../Config/connDB.php");
+    if ($_POST["key"]==KEY){
+        $id = $_POST["id"];
+        $conn->query("DELETE FROM `education` WHERE `id` = '$id'");
+        echo "200";
+    }
+}
+
+?>
