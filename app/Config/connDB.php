04484be1ae7d2f323155086cde18d72816447c69
mostafa hafezypoor <m.folad1383@gmail.com>
2026-01-29 09:49:54 +0330

    update

--- app/Config/connDB.php
+++ app/Config/connDB.php
@@ -1,6 +1,8 @@
 <?php
 define("KEY","test");
 define ("WEB_ADDRESS","http://192.168.1.2/ahmadAskarinia/");
+define("EITAA_CHANNEL_ID","11021712");
+define("EITAA_TOKEN_BOT","bot67363:e183e060-d00d-457b-8bb4-de37f1a8da64");
 $servername="localhost";
 $username="root";
 $password="";
