3b7b1cc5cd728b4eb2062d2e9df36fd35fdb3fdd
mostafa hafezypoor <m.folad1383@gmail.com>
2026-02-21 16:56:20 +0330

    update

--- index.php
+++ index.php
@@ -8,9 +8,9 @@ require_once ("app/Config/connDB.php");
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Profile &mdash; احمد عسکرینیا</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
-	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
-	<meta name="author" content="freehtml5.co" />
+	<meta name="description" content="3d designer" />
+	<meta name="keywords" content="3d,ahmmad askraynia,احمد عسکرینیا" />
+	<meta name="author" content="ahmmad askraynia,احمد عسکرینیا" />
 	<meta property="og:title" content=""/>
 	<meta property="og:image" content=""/>
 	<meta property="og:url" content=""/>
